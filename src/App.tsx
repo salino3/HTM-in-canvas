2e33979ca8a1d5448372a611fb1f8dce9a3f9b2c
Salino3 <flaviobcn012@gmail.com>
2026-04-16 15:32:40 +0200

    add fov Field of View

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { FormOnWall } from "./components";
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh", background: "#111" }}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
+      {/* fov: Field of View */}
+      <Canvas camera={{ position: [-2, 1, 5], fov: 80 }}>
         <ambientLight intensity={1} />
         <FormOnWall />
       </Canvas>
