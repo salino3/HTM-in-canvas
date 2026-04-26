e69382e8c41e2c43cd18131ca91fa790868c8a2c
Salino3 <flaviobcn012@gmail.com>
2026-04-26 12:13:24 +0200

    fix value input

--- src/components/form-on-wall/form-on-wall.tsx
+++ src/components/form-on-wall/form-on-wall.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState, type Dispatch, type SetStateAction } from "react";
 import * as THREE from "three";
 import { Html } from "@react-three/drei";
 import styles from "./form-on-wall.module.scss";
+import type { RoomValues } from "../room/room.component";
 
-export const FormOnWall: React.FC = () => {
+interface Props {
+  roomValues: RoomValues;
+  setRoomValues: Dispatch<SetStateAction<RoomValues>>;
+}
+
+interface DataForm {
+  shadeRoom: 1;
+  label: "";
+}
+
+export const FormOnWall: React.FC<Props> = ({ roomValues, setRoomValues }) => {
+  const [dataForm, setDataForm] = useState<DataForm>({
+    shadeRoom: 1,
+    label: "",
+  });
+
+  const handleFormData = (key: keyof DataForm) => (e: any) => {
+    const { value } = e.target;
+
+    setDataForm((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  //
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    if (dataForm.label) {
+      setRoomValues(dataForm);
+      setDataForm({
+        shadeRoom: 1,
+        label: "",
+      });
+    }
+  }
+
   return (
     // <group rotation={[0, -Math.PI / 4, 0]}> // Give it rotation
     // to keep it flat against the wall
@@ -19,17 +57,44 @@ export const FormOnWall: React.FC = () => {
         />
       </mesh>
       {/* 2. The HTML Layer */}
+
       <Html
         transform // This is the magic: it makes the HTML act like a 3D object
         occlude // (Optional) Makes it go behind other 3D objects
         distanceFactor={5} // Adjusts the "scale" of the HTML relative to 3D units
-        position={[0, 0, 0.01]} // Slightly in front of the wall to prevent flickering
+        position={[0, 0, 0.1]} // Slightly in front of the wall to prevent flickering
+        pointerEvents="auto"
+        className={styles.html}
       >
-        <form className={styles.formContainer}>
+        <h2>Label: {roomValues.label}</h2>
+        <form
+          onSubmit={handleSubmit}
+          // Stop the click from "bubbling" up to the OrbitControls
+          // onPointerDown={(e) => e.stopPropagation()}
+          className={styles.formContainer}
+        >
           <h1 className="title">Room Settings</h1>
-          <input type="range" min="0" max="10" />
-          <input type="text" placeholder="Wall Label..." />
-          <button className={styles["button"]} onClick={() => alert("Saved!")}>
+          <label htmlFor="shadeRoom">Shade Room</label>
+          <input
+            id="shadeRoom"
+            name="shadeRoom"
+            type="range"
+            min="0"
+            max="20"
+            value={dataForm.shadeRoom}
+            onChange={handleFormData("shadeRoom")}
+            // don't turn the room using range input
+            onPointerDown={(e) => e.stopPropagation()}
+          />
+          <label htmlFor="label">Label</label>
+          <input
+            type="text"
+            onChange={handleFormData("label")}
+            name="label"
+            value={dataForm.label}
+            placeholder="Wall Label..."
+          />
+          <button className={styles["button"]} type="submit">
             Save Changes
           </button>
         </form>
--- src/components/room/room.component.tsx
+++ src/components/room/room.component.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import { OrbitControls } from "@react-three/drei";
+import { Html, OrbitControls } from "@react-three/drei";
 import { FormOnWall } from "../form-on-wall";
 import "./room.styles.scss";
 
+export interface RoomValues {
+  shadeRoom: number;
+  label: string;
+}
+
 export const Room: React.FC = () => {
+  const [roomValues, setRoomValues] = useState<RoomValues>({
+    shadeRoom: 1,
+    label: "...",
+  });
+
   return (
     <div className="rootRoom">
       {/* fov: Field of View   */}
@@ -12,7 +23,7 @@ export const Room: React.FC = () => {
         <color attach="background" args={["#111"]} />
         <fog attach="fog" args={["#ffb115", -2, 30]} />
         <ambientLight
-          intensity={1}
+          intensity={roomValues.shadeRoom}
           //  color="white"
         />
         {/* Add a PointLight to act like a lightbulb in the room */}
@@ -25,14 +36,14 @@ export const Room: React.FC = () => {
         <mesh scale={[10, 10, 10]}>
           <boxGeometry />
           <meshStandardMaterial
-            color="#c81c1c"
+            color={"#c81c1c"}
             side={THREE.BackSide} //   Render the inside walls
           />
         </mesh>
 
         {/* THE FORM: Mounted on the back wall (z = -5) */}
         <group position={[0, 0, -4.9]}>
-          <FormOnWall />
+          <FormOnWall roomValues={roomValues} setRoomValues={setRoomValues} />
         </group>
 
         {/* Allows you to drag the mouse to look around the room */}
